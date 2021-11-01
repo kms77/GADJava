3679770db7e379f42dd220c823d0bf1bc0ea780c
Komșa Attila <63514374+kms77@users.noreply.github.com>
2021-11-01 21:00:15 +0200

    First exercise

--- src/Laboratory1/Exercises.java
+++ src/Laboratory1/Exercises.java
@@ -6,7 +6,7 @@ public class Exercises {
         char[] copyTo = new char[7];
         System.arraycopy(copyFrom, 2, copyTo, 0, 7);
         System.out.println("copyTo: " + new String(copyTo));
-        char [] copyToSecond = java.util.Arrays.copyOfRange(copyFrom, 2, 7);
-        System.out.println("copyToSecond: " + new String(copyTo));
+        char [] copyToSecond = java.util.Arrays.copyOfRange(copyFrom, 2, 9);
+        System.out.println("copyToSecond: " + new String(copyToSecond));
     }
 }
